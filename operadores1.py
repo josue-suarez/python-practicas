f8406a1108a01760e0eab382e4c6f2027b04b222
Josué Suárez <josuemsuarez@gmail.com>
2024-05-03 14:10:26 +0200

    v2.0

--- operadores1.py
+++ operadores1.py
@@ -2,10 +2,9 @@
 # entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3
 
 for i in range(10, 56):
-    if i % 2 == 0:
-        if i != 16 and i % 3 != 0:
-            print(i)
-        else:
-            pass
+    if i % 2 == 0 and i != 16 and i % 3 != 0:
+        print(i)
     else:
         pass
+    
+   
