8a6dc5eed2c8d3d2d61d05466fbcd254e3afa21d
Josué Suárez <josuemsuarez@gmail.com>
2024-05-03 14:06:54 +0200

    v1.0

---
+++ operadores1.py
@@ -0,0 +1,11 @@
+# Crea un programa que imprima por consola todos los números comprendidos
+# entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3
+
+for i in range(9, 55):
+    if i % 2 == 0:
+        if i != 16 and i % 3 != 0:
+            print(i)
+        else:
+            pass
+    else:
+        pass
