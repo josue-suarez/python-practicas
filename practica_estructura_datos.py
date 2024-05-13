81da71c0b88510bf4e9252050963184a4a736d58
Josué Suárez <josuemsuarez@gmail.com>
2024-05-13 17:14:12 +0200

    Finalizado

--- practica_estructura_datos.py
+++ practica_estructura_datos.py
@@ -8,16 +8,35 @@
 #   de 11 dígitos (o el número de dígitos que quieras).
 # - También se debe proponer una operación de finalización del programa.
 
+def buscar_contacto():
 
-
+def nuevo_contacto(ls_nombre, ln_telefono):
+    print("INTRODUCIR NUEVO CONTACTO")
+    ls_nombre = input("Introduzca nombre:")
+    ln_telefono = int(input("Introduzca teléfono:"))
+    main()
 
 def main():
+    dict agenda()
+    
     print("AGENDA DE CONTACTOS")
-    int(input("""Elija una opción:
+    opcion = int(input("""Elija una opción:
           1. Buscar contacto
           2. Nuevo contacto
           3. Borrar contacto
+          4. Salir
           Ingrese opción: """))
+    
+    if opcion == 1:
+        buscar_contacto()
+    elif opcion == 2:
+        nuevo_contacto()
+    elif opcion == 3:
+        borrar_contacto()
+    elif opcion == 4:
+        
+    else:
+        opcion_incorrecta()
     
 
 if __name__ == "__main__":
